12a23b46199b2ca95ef7121a2ba0d2e2e1fbd319
MuhammadQasim6 <139378308+MuhammadQasim6@users.noreply.github.com>
2023-07-13 10:56:13 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://proinsta.online/graphql/"
+	const endpoint = "https://newsseens.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://proinsta.online/` + encodeURI(path as string)
+					`https://newsseens.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
